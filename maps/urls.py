590350902ef5e129e3e6196b9bb086afc3be853d
eggseal <nicosm2004@gmail.com>
2024-11-12 19:24:49 -0500

    Requisitos y advertencias

--- maps/urls.py
+++ maps/urls.py
@@ -4,5 +4,5 @@ from .views import MapView, ProductJsonView
 
 urlpatterns = [
     path('', MapView.as_view(), name='map_view'),
-    path('/products-json/', ProductJsonView.as_view(), name='products_json'),
+    path('products-json/', ProductJsonView.as_view(), name='products_json'),
 ]
